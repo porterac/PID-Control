49342c1ec50b425d4926a09b321c5d473fac74a7
porterac <aporte29@byu.edu>
2025-08-15 17:21:38 -0600

    further understanding

--- PID Control/testing.py
+++ PID Control/testing.py
@@ -2,21 +2,16 @@ from pynput import keyboard
 
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        print('alphanumeric key {0} pressed'.format(key.char))
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        print('special key {0} pressed'.format(key))
 
 def on_release(key):
-    print('{0} released'.format(
-        key))
+    print(f'{key} released')
     if key == keyboard.Key.esc:
         # Stop listener
         return False
 
 # Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+with keyboard.Listener( on_press=on_press, on_release=on_release) as listener:
+     listener.join()
